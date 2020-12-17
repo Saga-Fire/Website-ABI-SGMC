3f872e2cabd1d76899bf0fd89bd8d9f742c6b161
Saga-Fire <75118944+Saga-Fire@users.noreply.github.com>
2020-12-17 15:46:03 +0100

    Operate data save

--- customerList.js
+++ customerList.js
@@ -5,18 +5,32 @@ document.getElementById("form").addEventListener("submit",(e)=>{
     const customerList = JSON.parse(sessionStorage.getItem('clients')) ? JSON.parse(sessionStorage.getItem('clients')) : [];
     const dataUser = [];
     const tbody = document.getElementById("newCustomer");
-    const tr = document.createElement('tr');
 
     console.log(customerList);
+    console.log(customerList[0].companyName)
 
-    // const customerTab = [companyName, clientNumber, clientType, clientActivity, clientTurnover, clientStaff, clientAdress, clientPhone, contact, comercialComments];
-
-    tbody.appendChild(tr);
-    for (let i = 0; i < 10; i++) {
-        const td = document.createElement('td');
-        td.innerHTML = customerList[i];
-        tr.appendChild(td);
-    }; 
+    for (let i = 0; i < customerList.length; i++) {
+        const customerTab = [
+            customerList[i].companyName,
+            customerList[i].clientNumber,
+            customerList[i].clientType,
+            customerList[i].clientActivity,
+            customerList[i].clientTurnover,
+            customerList[i].clientStaff,
+            customerList[i].clientAdress,
+            customerList[i].clientPhone,
+            customerList[i].contact,
+            customerList[i].comercialComments
+        ];
+        const tr = document.createElement('tr');
+        tbody.appendChild(tr);
+        for (let j = 0; j < customerTab.length; j++) {
+            const td = document.createElement('td');
+            td.innerHTML = customerTab[j];
+            tr.appendChild(td);
+        }; 
+    };
+    
     
 })
 
